af9e68e45e7f6169c6d40ce72c16d2846e8d86cd
SSlisko <135099622+SSlisko@users.noreply.github.com>
2024-06-17 14:46:34 +0200

    added Kioskapp project

---
+++ KioskServerApp/KioskServerApp/Models/Categories.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("price")]
+    public decimal Price { get; set; }
+}
+
+public class Category
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("products")]
+    public List<Product> Products { get; set; }
+}
